13364a68587d29903da33fe493e800ff0a280337
Dipak Pulami Magar <dpm.it24800@gmail.com>
2026-04-01 12:09:34 +0545

    Initialize Streamlit app with home page content

---
+++ index.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Home Page", page_icon="🏠")
+
+st.title("Welcome to my Multi-Page App")
+st.write("Use the sidebar on the left to navigate between different sections.")
+
+st.info("Files inside the 'pages' folder automatically show up as navigation links!")
